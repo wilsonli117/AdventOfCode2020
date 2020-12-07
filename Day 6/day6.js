4a76154b6a138130cd6a2c659677bd98a2faf5e1
WilsonLi <ligokdoon@gmail.com>
2020-12-07 15:49:37 -0500

    day 6

---
+++ Day 6/day6.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const customsAnswers = fs.readFileSync("./customs.txt").toString();
+let customsAnswersByGroup = customsAnswers.split("\n\n");
+
+let countSum = 0;
+
+for(let i = 0; i < customsAnswersByGroup.length; i++) {
+    let groupAnswers = customsAnswersByGroup[i].split('\n').join('');
+    let groupQuestions = {};
+    let groupCount = 0;
+
+    for(let k = 0; k < groupAnswers.length; k++) {
+        if (!groupQuestions[groupAnswers[k]]) {
+            groupQuestions[groupAnswers[k]] = true;
+            groupCount += 1;
+        }
+    }
+    
+    countSum += groupCount;
+}
+
+console.log(countSum);
