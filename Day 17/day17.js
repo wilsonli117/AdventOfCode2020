5abaeeb4a127a0c3ae425118d47bde587cd84ab3
WilsonLi <ligokdoon@gmail.com>
2020-12-30 20:50:32 -0500

    day 17 pt 1

--- Day 17/day17.js
+++ Day 17/day17.js
@@ -12,6 +12,41 @@ findActiveCubes(initialState, 1);
 
 function runRycle(grid) {
     let z = 0;
+    let slice = grid[z];
 
+    for(let i = 0; i < slice.length; i++) {
+        let row = slice[i];
 
+        row.forEach((cube, idx) => {
+            let activeCount = 0;
+
+            if (row[idx - 1] === '#') activeCount += 1; //left
+            if (row[idx + 1] === '#') activeCount += 1; //right
+            if (slice[i - 1][idx] === '#') activeCount += 1; //above
+            if (slice[i - 1][idx - 1] === '#') activeCount += 1; //top-left diagonal
+            if (slice[i - 1][idx + 1] === '#') activeCount += 1; //top-right diagonal
+            if (slice[i + 1][idx] === '#') activeCount += 1; //below
+            if (slice[i + 1][idx - 1] === '#') activeCount += 1; //bottom-left diagonal
+            if (slice[i + 1][idx + 1] === '#') activeCount += 1; //bottom-right diagonal
+            if (grid[z - 1][i][idx]) activeCount += 1; //previous slice, same position
+            if (grid[z - 1][i][idx - 1]) activeCount += 1; //previous slice, left
+            if (grid[z - 1][i][idx + 1]) activeCount += 1; //previous slice, right
+            if (grid[z - 1][i - 1][idx]) activeCount += 1; //previous slice, above
+            if (grid[z - 1][i - 1][idx - 1]) activeCount += 1; //previous slice, top-left diagonal
+            if (grid[z - 1][i - 1][idx + 1]) activeCount += 1; //previous slice, top-right diagonal
+            if (grid[z - 1][i + 1][idx]) activeCount += 1; //previous slice, below
+            if (grid[z - 1][i + 1][idx - 1]) activeCount += 1; //previous slice, bottom-left diagonal
+            if (grid[z - 1][i + 1][idx + 1]) activeCount += 1; //previous slice, bottom-right diagonal
+            if (grid[z + 1][i][idx]) activeCount += 1; //next slice, same position
+            if (grid[z + 1][i][idx - 1]) activeCount += 1; //next slice, left
+            if (grid[z + 1][i][idx + 1]) activeCount += 1; //next slice, right
+            if (grid[z + 1][i - 1][idx]) activeCount += 1; //next slice, above
+            if (grid[z + 1][i - 1][idx - 1]) activeCount += 1; //next slice, top-left diagonal
+            if (grid[z + 1][i - 1][idx + 1]) activeCount += 1; //next slice, top-right diagonal
+            if (grid[z + 1][i + 1][idx]) activeCount += 1; //next slice, below
+            if (grid[z + 1][i + 1][idx - 1]) activeCount += 1; //next slice, bottom-left diagonal
+            if (grid[z + 1][i + 1][idx + 1]) activeCount += 1; //next slice, bottom-right diagonal
+
+        })
+    }
 }
